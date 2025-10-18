d4e8b192f09041f515626c68dc4e2a1dcc299546
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-18 12:55:28 +0200

    lorem

--- internal/handlers/homehandler/homehandler.go
+++ internal/handlers/homehandler/homehandler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/loissascha/go-http-server/server"
+	"github.com/loissascha/go-templ-template/internal/ui/components"
 	"github.com/loissascha/go-templ-template/internal/ui/layouts"
 	"github.com/loissascha/go-templ-template/internal/ui/pages"
 )
@@ -17,10 +18,15 @@ func New() *HomeHandler {
 
 func (h *HomeHandler) RegisterHandlers(s *server.Server) {
 	s.GET("/", h.homeRoute)
+	s.GET("/lorem", h.loremIpsumRoute)
 }
 
 func (h *HomeHandler) homeRoute(w http.ResponseWriter, r *http.Request) {
 	homeComponent := pages.Home()
 	layoutComponent := layouts.Layout(homeComponent)
 	layoutComponent.Render(r.Context(), w)
+}
+
+func (h *HomeHandler) loremIpsumRoute(w http.ResponseWriter, r *http.Request) {
+	components.Lorem().Render(r.Context(), w)
 }
